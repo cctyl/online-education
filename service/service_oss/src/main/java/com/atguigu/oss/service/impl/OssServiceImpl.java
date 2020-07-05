3eb27001a4f2d47004b99f379e4466587fe6eb40
cctyl <2621460357@qq.com>
2020-07-05 15:09:37 +0800

    添加：文件分类功能

--- service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
@@ -1,11 +1,13 @@
 package com.atguigu.oss.service.impl;
 
+import cn.hutool.core.date.DateUtil;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.commonutils.ExceptionUtil;
 import com.atguigu.oss.service.OssService;
 import com.atguigu.oss.utils.ConstantProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -20,7 +23,7 @@ public class OssServiceImpl implements OssService {
 
     @Autowired
     ConstantProperties constantProperties;
-    String basePath ="https://"+ constantProperties.getBucketname()+"."+constantProperties.getEndpoint()+"/";
+
 
     /**
      * 上传头像到oss中，返回头像url
@@ -31,7 +34,7 @@ public class OssServiceImpl implements OssService {
     @Override
     public String uploadAvatar(MultipartFile file) {
 
-
+        String basePath ="https://"+ constantProperties.getBucketname()+"."+constantProperties.getEndpoint()+"/";
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = constantProperties.getEndpoint();
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
@@ -39,8 +42,12 @@ public class OssServiceImpl implements OssService {
         String accessKeySecret = constantProperties.getKeysecret();
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        //获取文件名
-        String originalFilename = file.getOriginalFilename();
+        //获取文件名,并且加上随机值
+        String originalFilename =UUID.randomUUID().toString().replaceAll("-","")+ file.getOriginalFilename();
+
+        //对文件进行分类存放，按照日期进行分类
+        String today = new DateTime().toString("yyyy/MM/dd");
+        String realPath =today+ "/"+originalFilename;
 
 
         try {
@@ -48,7 +55,7 @@ public class OssServiceImpl implements OssService {
             InputStream inputStream = file.getInputStream();
             // 第一个参数  bucketname
             //第二个参数   文件名和路径
-            ossClient.putObject(constantProperties.getBucketname(), originalFilename, inputStream);
+            ossClient.putObject(constantProperties.getBucketname(), realPath, inputStream);
         } catch (IOException e) {
 
             log.error(ExceptionUtil.getMessage(e));
@@ -60,7 +67,7 @@ public class OssServiceImpl implements OssService {
 
         //拼接路径
         // https://bucketname.endpoint/文件路径
-        String url = basePath+originalFilename;
+        String url = basePath+realPath;
 
         return url;
     }
