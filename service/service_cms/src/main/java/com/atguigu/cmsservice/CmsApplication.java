55eea89dceee90347ae2a2ebd75e639822635e34
cctyl <2621460357@qq.com>
2020-08-12 09:43:25 +0800

    cms服务加入nacos

--- service/service_cms/src/main/java/com/atguigu/cmsservice/CmsApplication.java
+++ service/service_cms/src/main/java/com/atguigu/cmsservice/CmsApplication.java
@@ -3,11 +3,13 @@ package com.atguigu.cmsservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan({"com.atguigu"}) //指定扫描位置
+@EnableDiscoveryClient
 public class CmsApplication {
 
 
