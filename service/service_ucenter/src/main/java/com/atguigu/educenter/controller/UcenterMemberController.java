df1758181bb846fcc03e2186b54334089e0777a3
cctyl <2621460357@qq.com>
2020-08-15 10:40:05 +0800

    完成用户登陆接口

--- service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
@@ -1,9 +1,12 @@
 package com.atguigu.educenter.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.service.UcenterMemberService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-08-15
  */
 @RestController
-@RequestMapping("/educenter/ucenter-member")
+@RequestMapping("/educenter/member")
+@CrossOrigin
 public class UcenterMemberController {
+
+
+
+    @Autowired
+    private UcenterMemberService memberService;
+
+
+    /**
+     * 用户登陆方法，需要传递手机号和密码
+     * @param member
+     * @return
+     */
+    @PostMapping("/login")
+    @ApiOperation("用户登陆")
+    public R login(@RequestBody UcenterMember member){
+
+
+        String token= memberService.login(member);
+
+        return R.ok().data("token",token);
+
+    }
+
+
+
+
 
 }
 
--- service/service_ucenter/src/main/java/com/atguigu/educenter/service/impl/UcenterMemberServiceImpl.java
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/service/impl/UcenterMemberServiceImpl.java
@@ -1,10 +1,16 @@
 package com.atguigu.educenter.service.impl;
 
+import com.atguigu.commonutils.JWTUtils;
 import com.atguigu.educenter.entity.UcenterMember;
 import com.atguigu.educenter.mapper.UcenterMemberMapper;
 import com.atguigu.educenter.service.UcenterMemberService;
+import com.atguigu.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.management.Query;
 
 /**
  * <p>
@@ -17,4 +23,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class UcenterMemberServiceImpl extends ServiceImpl<UcenterMemberMapper, UcenterMember> implements UcenterMemberService {
 
+    /**
+     * 用户登陆
+     * @param loginmember
+     * @return token
+     */
+    @Override
+    public String login(UcenterMember loginmember) {
+
+
+        String mobile = loginmember.getMobile();
+        String password = loginmember.getPassword();
+        Boolean isDisabled = loginmember.getIsDisabled();
+
+        //手机号和密码都不能为空
+        if (StringUtils.isEmpty(mobile)|| StringUtils.isEmpty(password)){
+            throw new GuliException(20001,"登陆失败");
+
+        }
+
+
+        QueryWrapper<UcenterMember> wrapper = new QueryWrapper<>();
+        wrapper.eq("mobile",mobile);
+
+        UcenterMember ucenterMember = baseMapper.selectOne(wrapper);
+        //手机号是否存在
+        if (ucenterMember==null){
+
+            throw new GuliException(20001,"登陆失败");
+        }
+
+       //密码是否正确
+        if ( !ucenterMember.getPassword().equals(password)) {
+            throw new GuliException(20001,"登陆失败");
+        }
+
+
+        //判断用户是否被禁用
+        if (ucenterMember.getIsDisabled()){
+            throw new GuliException(20001,"登陆失败");
+        }
+
+
+        //到这里之后，登陆成功
+        //生成token,把用户id和用户昵称作为数据生成token
+        String jwtToken = JWTUtils.getJwtToken(ucenterMember.getId(), ucenterMember.getNickname());
+
+
+        return jwtToken;
+    }
 }
