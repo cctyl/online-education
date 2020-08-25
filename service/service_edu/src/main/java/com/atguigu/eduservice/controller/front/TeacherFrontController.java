69124b3e4ff4b8c876f99846382e547daf9e3fed
cctyl <2621460357@qq.com>
2020-08-25 10:37:49 +0800

    实现前端讲师详情查询接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
@@ -1,13 +1,17 @@
 package com.atguigu.eduservice.controller.front;
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduCourseService;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,6 +22,12 @@ public class TeacherFrontController {
 
     @Autowired
     private EduTeacherService eduTeacherService;
+
+
+    @Autowired
+    private EduCourseService courseService;
+
+
 
     /**
      * 分页查询讲师
@@ -35,6 +45,29 @@ public class TeacherFrontController {
         Map<String, Object> map = eduTeacherService.getTeacherFrontList(pageBean);
 
         return R.ok().data(map);
+    }
+
+
+    /**
+     * 获取讲师详情信息
+     * @param teacherId
+     * @return
+     */
+    @GetMapping("/info/{teacherId}")
+    @ApiOperation("前台获取讲师详情信息")
+    public R getTeacherInfo(@PathVariable("teacherId") String teacherId){
+
+        //1.查询讲师详情
+        EduTeacher eduTeacher = eduTeacherService.getById(teacherId);
+
+
+        //2.查询讲师讲过的课程信息
+        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
+        wrapper.eq("teacher_id",teacherId);
+        List<EduCourse> list = courseService.list(wrapper);
+
+        return R.ok().data("teacher",eduTeacher).data("courseList",list);
+
     }
 
 }
