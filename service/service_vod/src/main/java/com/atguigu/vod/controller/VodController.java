29ce7f12f205c42d8e3f06c78f10ef3872a85957
cctyl <2621460357@qq.com>
2020-08-30 10:47:42 +0800

    添加视频播放凭证获取接口

--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -1,7 +1,15 @@
 package com.atguigu.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
+import com.atguigu.commonutils.ExceptionUtil;
 import com.atguigu.commonutils.R;
+import com.atguigu.exceptionhandler.GuliException;
 import com.atguigu.vod.service.VodService;
+import com.atguigu.vod.utils.ConstantProperties;
+import com.atguigu.vod.utils.InitVodClient;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.ibatis.annotations.Delete;
@@ -18,6 +26,10 @@ public class VodController {
 
     @Autowired
     VodService vodService;
+
+
+    @Autowired
+    ConstantProperties constantProperties;
 
     /**
      * 将视频上传到阿里云，返回一个视频id
@@ -60,5 +72,37 @@ public class VodController {
         return R.ok();
 
     }
+
+    /**
+     * 根据视频id获取凭证
+     * @param id
+     * @return
+     */
+    @GetMapping("/playAuth/{id}")
+    public R getPlayInfo(@PathVariable("id") String id){
+
+
+        try {
+            DefaultAcsClient defaultAcsClient = InitVodClient.initVodClient(constantProperties.getKeyid(), constantProperties.getKeysecret());
+
+
+            //创建获取视频地址request 和 response
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            GetVideoPlayAuthResponse response = new GetVideoPlayAuthResponse();
+
+            //给请求添加参数：设置id
+            request.setVideoId(id);
+
+            //执行请求后获得响应
+            response = defaultAcsClient.getAcsResponse(request);
+            String playAuth = response.getPlayAuth();
+            return R.ok().data("playAuth",playAuth);
+
+        } catch (ClientException e) {
+            throw new GuliException(20001,"获取凭证失败:"+ ExceptionUtil.getMessage(e));
+        }
+
+    }
+
 
 }
