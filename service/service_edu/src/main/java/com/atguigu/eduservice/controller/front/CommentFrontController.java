89e51c23c326037e842fa478daa27ce95146ff84
cctyl <2621460357@qq.com>
2020-09-02 10:06:25 +0800

    添加课程评论查询接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/CommentFrontController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/CommentFrontController.java
@@ -1,9 +1,16 @@
 package com.atguigu.eduservice.controller.front;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduComment;
+import com.atguigu.eduservice.service.EduCommentService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -17,6 +24,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/eduservice/comment/front")
 @CrossOrigin
 public class CommentFrontController {
+
+    @Autowired
+    EduCommentService eduCommentService;
+
+    /**
+     * 分页查询评论
+     *
+     * @param page  当前页码
+     * @param limit 每页记录数
+     * @return
+     */
+    @PostMapping("/{page}/{limit}")
+    @ApiOperation("分页查询评论")
+    public R getCommentList(@ApiParam(name = "page", value = "当前页码", required = true)
+                            @PathVariable("page") Integer page,
+
+                            @ApiParam(name = "limit", value = "每页记录数", required = true)
+                            @PathVariable("limit") Long limit) {
+
+        Page<EduComment> eduCommentPage = new Page<>(page,limit);
+
+      Map<String,Object> map =  eduCommentService.getCommentList(eduCommentPage);
+
+      return R.ok().data(map);
+    }
+
 
 }
 
