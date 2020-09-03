8ac1c53dd58c80931e34de1189792a2600ca9f8d
cctyl <2621460357@qq.com>
2020-09-03 09:29:16 +0800

    修复：is_deleted字段类型转换异常的bug

--- service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduComment.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduComment.java
@@ -49,7 +49,7 @@ public class EduComment implements Serializable {
     private String content;
 
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
-    private Boolean isDeleted;
+    private Integer isDeleted;
 
     @ApiModelProperty(value = "创建时间")
     private Date gmtCreate;
