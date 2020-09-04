1b7df1352014d01b3d1df3763d8411044a9bc81b
cctyl <2621460357@qq.com>
2020-09-04 10:42:43 +0800

    修复：is_deleted字段类型转换异常的bug

--- service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduComment.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduComment.java
@@ -1,9 +1,10 @@
 package com.atguigu.eduservice.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -49,13 +50,16 @@ public class EduComment implements Serializable {
     private String content;
 
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
     private Integer isDeleted;
 
+    @TableField(fill = FieldFill.INSERT)
     @ApiModelProperty(value = "创建时间")
     private Date gmtCreate;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(value = "更新时间")
     private Date gmtModified;
-
 
 }
