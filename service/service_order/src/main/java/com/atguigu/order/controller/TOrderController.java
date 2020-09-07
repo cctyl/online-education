548acb97f1087fcb9b61a3acfd7165118bec13be
cctyl <2621460357@qq.com>
2020-09-07 11:14:51 +0800

    完善订单查询接口

--- service/service_order/src/main/java/com/atguigu/order/controller/TOrderController.java
+++ service/service_order/src/main/java/com/atguigu/order/controller/TOrderController.java
@@ -3,7 +3,9 @@ package com.atguigu.order.controller;
 
 import com.atguigu.commonutils.JWTUtils;
 import com.atguigu.commonutils.R;
+import com.atguigu.order.entity.TOrder;
 import com.atguigu.order.service.TOrderService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -49,6 +51,13 @@ public class TOrderController {
     @GetMapping("/{orderId}")
     @ApiOperation("根据id查询订单信息")
     public R getOrderInfo(@PathVariable("orderId") String  orderId){
+
+        QueryWrapper<TOrder> wrapper = new QueryWrapper<>();
+        wrapper.eq("order_no",orderId);
+        TOrder order = orderService.getOne(wrapper);
+
+        return R.ok().data("item",order);
+
 
 
 
