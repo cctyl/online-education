5471096dbd074bb8d3708de7f91f8e7d53dd2647
cctyl <2621460357@qq.com>
2020-09-27 21:24:28 +0800

    添加：完成数据查询接口

--- service/service_statistics/src/main/java/com/atguigu/statistics/controller/DailyController.java
+++ service/service_statistics/src/main/java/com/atguigu/statistics/controller/DailyController.java
@@ -2,6 +2,7 @@ package com.atguigu.statistics.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.statistics.entity.Daily;
 import com.atguigu.statistics.service.DailyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +19,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/statistics/daily")
 public class DailyController {
 
+    @Autowired
+    private DailyService dailyService;
 
     /**
      * 根据日期查询统计数据
+     *
      * @param day
      * @return
      */
     @GetMapping("/{day}")
-    public R getDailyCount(@PathVariable("day") String day){
+    public R getDailyCount(@PathVariable("day") String day) {
 
 
+        Daily daily = dailyService.getDailyCountByDate(day);
+        return R.ok().data("item",daily);
     }
-
 
 
 }
