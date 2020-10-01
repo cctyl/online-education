4310afbb1860f4aab672937970841fb6106b4a48
cctyl <2621460357@qq.com>
2020-10-01 10:20:38 +0800

    完善数据查询接口

--- service/service_statistics/src/main/java/com/atguigu/statistics/service/impl/DailyServiceImpl.java
+++ service/service_statistics/src/main/java/com/atguigu/statistics/service/impl/DailyServiceImpl.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,9 +29,6 @@ import java.util.Map;
 public class DailyServiceImpl extends ServiceImpl<DailyMapper, Daily> implements DailyService {
 
 
-
-
-
     @Override
     public void dailyCount(Daily daily) {
         baseMapper.insert(daily);
@@ -37,13 +37,48 @@ public class DailyServiceImpl extends ServiceImpl<DailyMapper, Daily> implements
 
     /**
      * 根据日期查询统计数据
+     *
      * @param begin
      * @param end
      * @return
      */
     @Override
     public Map<String, Object> getDailyCount(String begin, String end) {
-        return null;
+
+        QueryWrapper<Daily> wrapper = new QueryWrapper<>();
+        wrapper.between("date_calculated", begin, end);
+        wrapper.select("date_calculated", "register_num", "login_num", "video_view_num", "course_num");
+
+        //查出想要的列
+        List<Daily> dailyList = baseMapper.selectList(wrapper);
+
+        //每一列单独封装成一个list
+        List<String> dateList = new ArrayList<>();//日期数组
+        List<Integer> registerList = new ArrayList<>(); //注册人数
+        List<Integer> loginList = new ArrayList<>();//登陆人数
+        List<Integer> viewList = new ArrayList<>();//播放次数
+        List<Integer> courseList = new ArrayList<>();//新增课程数
+
+        for (Daily temp :
+                dailyList) {
+
+            dateList.add(temp.getDateCalculated());
+            registerList.add(temp.getRegisterNum());
+            loginList.add(temp.getLoginNum());
+            viewList.add(temp.getVideoViewNum());
+            courseList.add(temp.getCourseNum());
+
+        }
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("dateList",dateList);
+        map.put("registerList",registerList);
+        map.put("loginList",loginList);
+        map.put("viewList",viewList);
+        map.put("courseList",courseList);
+
+        return map;
     }
 
 
