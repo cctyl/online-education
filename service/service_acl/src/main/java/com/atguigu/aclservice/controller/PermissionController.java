265bf426033aa3ce03d3217605530a44e4796e75
cctyl <2621460357@qq.com>
2020-10-14 16:03:57 +0800

    添加权限查询接口

--- service/service_acl/src/main/java/com/atguigu/aclservice/controller/PermissionController.java
+++ service/service_acl/src/main/java/com/atguigu/aclservice/controller/PermissionController.java
@@ -26,50 +26,20 @@ public class PermissionController {
     @Autowired
     private PermissionService permissionService;
 
-    //获取全部菜单
-    @ApiOperation(value = "查询所有菜单")
-    @GetMapping
-    public R indexAllPermission() {
-        List<Permission> list =  permissionService.queryAllMenuGuli();
-        return R.ok().data("children",list);
+
+    /**
+     * 查询所有菜单
+     *
+     * @return
+     */
+    @GetMapping("/all")
+    @ApiOperation("查询所有菜单")
+    public R getAllPermisson() {
+        List<Permission> permissionList = permissionService.getAllPermisson();
+        return R.ok().data("list", permissionList);
+
     }
 
-    @ApiOperation(value = "递归删除菜单")
-    @DeleteMapping("remove/{id}")
-    public R remove(@PathVariable String id) {
-        permissionService.removeChildByIdGuli(id);
-        return R.ok();
-    }
-
-    @ApiOperation(value = "给角色分配权限")
-    @PostMapping("/doAssign")
-    public R doAssign(String roleId,String[] permissionId) {
-        permissionService.saveRolePermissionRealtionShipGuli(roleId,permissionId);
-        return R.ok();
-    }
-
-    @ApiOperation(value = "根据角色获取菜单")
-    @GetMapping("toAssign/{roleId}")
-    public R toAssign(@PathVariable String roleId) {
-        List<Permission> list = permissionService.selectAllMenu(roleId);
-        return R.ok().data("children", list);
-    }
-
-
-
-    @ApiOperation(value = "新增菜单")
-    @PostMapping("save")
-    public R save(@RequestBody Permission permission) {
-        permissionService.save(permission);
-        return R.ok();
-    }
-
-    @ApiOperation(value = "修改菜单")
-    @PutMapping("update")
-    public R updateById(@RequestBody Permission permission) {
-        permissionService.updateById(permission);
-        return R.ok();
-    }
 
 }
 
